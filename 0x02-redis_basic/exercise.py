cbc69c6be6e2fa9fe42f23f0d1092595aa1b3653
kelvin defrNBAH <kevinocholah@gmail.com>
2024-08-15 06:00:13 +0300

    redis ini

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,7 +21,7 @@ and an optional Callable argument named fn. This callable will be used to
 convert the data back to the desired format.
 
 Remember to conserve the original Redis.get behavior if the key does not exist.
-Also, implement 2 new methods: get_str and get_int that will automatically
+Also, implement : methods: get_str and get_int that will automatically
 parametrize Cache.get with the correct conversion function.
 """
 
